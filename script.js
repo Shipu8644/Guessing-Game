d1728fdedb34edcddee762f7b7f8c2acdcc0e637
Shipu8644 <shipu.monjurur@gmail.com>
2021-08-30 22:21:41 +0600

    first

---
+++ script.js
@@ -0,0 +1,28 @@
+const getGuessValue = () => {
+    const guessNumber = document.getElementById('guess-number');
+    guessNumber.innerText = Math.round(Math.random() * 20);
+    return (guessNumber.innerText);
+}
+
+const checkButton = () => {
+    const guessInput = document.getElementById('guess-input');
+    const guessInputValue = guessInput.value;
+    // console.log(guessInputValue);
+    const guessNumberText = getGuessValue();
+    if (guessNumberText == guessInputValue) {
+        document.body.style.backgroundColor = 'green';
+        document.getElementById('clue').innerText = 'Correct Number!!!'
+    }
+    else {
+        document.getElementById('guess-number').innerText = '?';
+        document.getElementById('score').innerText--;
+    }
+
+}
+const Clear = () => {
+    document.body.style.backgroundColor = '#222';
+    document.getElementById('guess-input').value = '';
+    document.getElementById('score').innerText = 20;
+    document.getElementById('guess-number').innerText = '?';
+    document.getElementById('clue').innerText = 'Start guessing...';
+}
